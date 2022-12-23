77cc073cb1edaf326b1d9dab809171a86ee7e9d6
John Thompson <john@springframework.guru>
2022-12-23 11:35:09 -0500

    add default sec chain

--- src/main/java/config/SecurityConfig.java
+++ src/main/java/config/SecurityConfig.java
@@ -35,4 +35,19 @@ public class SecurityConfig {
 
         return http.build();
     }
+
+    @Bean
+    @Order(2)
+    public SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http)
+            throws Exception {
+        http
+                .authorizeHttpRequests((authorize) -> authorize
+                        .anyRequest().authenticated()
+                )
+                // Form login handles the redirect to the login page from the
+                // authorization server filter chain
+                .formLogin(Customizer.withDefaults());
+
+        return http.build();
+    }
 }
